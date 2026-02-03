0ed20c7a1159236eb66c452456139d24ccbfd3e0
Justin <jchen73@stuy.edu>
2026-02-02 22:21:21 -0500

    Completed robot drawing.

--- classwork/2026-2-2.py
+++ classwork/2026-2-2.py
@@ -1,22 +1,71 @@
 def setup(): #runs once
     size(400, 400)
  
-def draw(): #runs like in forever loop in Netlogo
-    #background(220) #make the background grey
-    #fill(255)
-    #circle(120,120,200)
-    #fill(220)
-    #no_stroke()
-    #rect(10,100,220,150)
-    #stroke(0)
-    #line(20,100,220,100)
-    background(220)
+def draw():
+    background(255)
     grid()
     displayCoordinates()
+    
+    # Robot drawing
+    stroke(0)
+    stroke_weight(3)
+    no_fill()
+    
+    # Antenna
+    circle(200, 60, 15)
+    line(200, 67, 200, 90)
+
+    stroke(0)
+    ellipse(200, 110, 60, 40)
+    no_stroke()
     fill(255)
-    rect(145, 185, 110, 70)
-    rect(120, 80, 160, 80)
-    rect()
+    rect(160, 110, 80, 25)
+    stroke(0)
+    line(170, 110, 230, 110)
+    no_fill()
+    
+    # Head
+    rect(120, 110, 160, 100)
+    
+    # Eyes
+    stroke_weight(4)
+    circle(160, 150, 40)
+    circle(240, 150, 40)
+    circle(160, 150, 15)
+    circle(240, 150, 15)
+    stroke_weight(3)
+    
+    # Neck
+    line(180, 210, 180, 230)
+    line(220, 210, 220, 230)
+    
+    # Body
+    rect(140, 230, 120, 100)
+    
+    # Arms
+    line(140, 240, 100, 290)
+    line(260, 240, 300, 290)
+    
+    # Chest Plate
+    rect(160, 245, 80, 30)
+    circle(200, 290, 15)
+    
+    # Legs
+    line(170, 330, 160, 350)
+    line(230, 330, 240, 350)
+    
+    # Feet
+    stroke(0)
+    no_fill()
+    circle(160, 370, 40)
+    circle(240, 370, 40)
+    no_stroke()
+    fill(255)
+    rect(130, 370, 60, 30)
+    rect(210, 370, 60, 30)
+    stroke(0)
+    line(140, 370, 180, 370)
+    line(220, 370, 260, 370)
 
 def grid():
     stroke(255)
