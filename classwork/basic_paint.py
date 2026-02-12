31a76d9ae5f789c22b5976471c2cfc0eb44cae91
Justin <jchen73@stuy.edu>
2026-02-11 20:50:54 -0500

    Finished stroke weight keys but can't test right now.

--- classwork/basic_paint.py
+++ classwork/basic_paint.py
@@ -14,11 +14,29 @@ def draw():
   stroke_weight(settings["stroke_weight"])
   fill(settings["fill"])
 
+  if (is_key_pressed()):
+    # stroke weights
+    if (key == "0"):
+      settings["stroke_weight"] = 1
+    elif (key == "1"):
+      settings["stroke_weight"] = 2
+    elif (key == "2"):
+      settings["stroke_weight"] = 3
+    elif (key == "3"):
+      settings["stroke_weight"] = 4
+    elif (key == "4"):
+      settings["stroke_weight"] = 5
+    elif (key == "5"):
+      settings["stroke_weight"] = 6
+    elif (key == "6"):
+      settings["stroke_weight"] = 7
+    elif (key == "7"):
+      settings["stroke_weight"] = 8
+    elif (key == "8"):
+      settings["stroke_weight"] = 9
+    elif (key == "9"):
+      settings["stroke_weight"] = 10
 
-def mouse_pressed():
-  line(mouse_x, mouse_y, pmouse_x, pmouse_y)
-        
-    
 def collidePointRect(pX, pY, rX, rY, rW, rH):
   """Input x,y coordinates of point and x, y, width, and height of rectangle.
   Returns true if the point and rectangle are touching."""
