5ae95b7381e95d124cd565df961c531b61e721d4
Justin <jchen73@stuy.edu>
2026-03-30 23:57:15 -0400

    Finished greeting card.

--- Holiday/Tile.py
+++ Holiday/Tile.py
@@ -1,21 +1,33 @@
 # PY5 IMPORTED MODE CODE
 class Tile:
-    def __init__(self, x, y, scal, direction, angle, letter):
-        rect_mode(CENTER)
+    def __init__(self, x, y, direction, angle, letter):
         self.x = x
         self.y = y
-        self.scal = scal
         self.dir = direction
         self.angle = angle
         self.letter = letter
-        self.opacity = 200
+        self.opacity = 60
+        self.hovered = False
+
     def checkDead(self):
         if self.y > height + 50:
             return True
         return False
+
+    def checkHover(self, x, y):
+        if abs(x - self.x) < 25 and abs(y - self.y) < 25:
+            self.hovered = True
+            self.opacity = 230
+        else:
+            self.hovered = False
+            self.opacity += (60 - self.opacity) * 0.15
+
     def display(self):
         self.angle -= 1
         self.y += 1
+        push_style()
+        rect_mode(CENTER)
+        text_align(CENTER, CENTER)
         push_matrix()
         translate(self.x, self.y)
         if self.dir == "left":
@@ -23,12 +35,16 @@ class Tile:
         elif self.dir == "right":
             rotate(-radians(self.angle))
         no_stroke()
-        fill(92, 79, 63, self.opacity) #shadow color
-        rect(-3, 3, 50, 50, 10)
-        stroke(253, 217, 169)
-        fill(253, 217, 169, self.opacity) #scrabble wood color
+
+        if self.hovered:
+            fill(92, 79, 63, self.opacity)
+            rect(-3, 3, 50, 50, 10)
+        
+        stroke(253, 217, 169, self.opacity)
+        fill(253, 217, 169, self.opacity)
         rect(0, 0, 50, 50, 10)
         fill(0, 0, 0, self.opacity)
         text_size(30)
-        text(self.letter, -8, 10)
+        text(self.letter, 0, -2)
         pop_matrix()
+        pop_style()
