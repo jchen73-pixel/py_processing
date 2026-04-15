4d5d2adea71b038a19b20061b2368e8c467af625
Justin <jchen73@stuy.edu>
2026-04-15 02:39:17 -0400

    homework.

--- Classwork/modRemainder.py
+++ Classwork/modRemainder.py
@@ -14,15 +14,23 @@ def draw():
     translate(width / 2, height / 2)
     circle(0, 0, 200)
     stroke_weight(2)
-    hourRad = remap(hour(), 0, 12, 0, TWO_PI)
-    minRad = remap(minute(), 0, 60, 0, TWO_PI)
-    secRad = remap(second(), 0, 60, 0, TWO_PI)
+    hourRad = remap(hour() % 12 + minute() / 60.0, 0, 12, 0, TWO_PI)
+    minRad  = remap(minute() + second() / 60.0, 0, 60, 0, TWO_PI)
+    secRad  = remap(second(), 0, 60, 0, TWO_PI)
     line(0, 0, 30 * cos(hourRad - HALF_PI), 30 * sin(hourRad - HALF_PI))
     line(0, 0, 60 * cos(minRad - HALF_PI), 60 * sin(minRad - HALF_PI))
     line(0, 0, 90 * cos(secRad - HALF_PI), 90 * sin(secRad - HALF_PI))
     stroke_weight(1)
+    text_align(CENTER, CENTER)
+    text_size(16)
     for i in range(12):
         tickRad = remap(i, 0, 12, 0, TWO_PI)
         line(90 * cos(tickRad), 90 * sin(tickRad), 100 * cos(tickRad), 100 * sin(tickRad))
-    
+        fill(0)
+        numRad = remap(i, 0, 12, 0, TWO_PI) - PI / 3
+        num = i + 1
+        if num == 13:
+            num = 1
+        text(str(num), 75 * cos(numRad), 75 * sin(numRad))
+        fill(255)
     pop_matrix()
