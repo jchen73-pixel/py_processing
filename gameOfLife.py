f3c6d6506ea9e69ff783c83b8b68e1bc0815b954
Justin Chen <jchen73@stuy.edu>
2026-05-20 09:25:59 -0400

    All py5 work

--- gameOfLife.py
+++ gameOfLife.py
@@ -10,13 +10,18 @@ def setup_pixels():
                 row.append(state[1])
             else:
                 row.append(state[0])
+            #row.append(state[0])
         p.append(row)
+    update_np_pixels()
+    
+def draw_glider():
+    p[3][1], p[3][2], p[3][3], p[2][3], p[1][2] = state[1], state[1], state[1], state[1], state[1]
+    transition()
     update_np_pixels()
     
 def setup():
     global p
     size(200, 200)
-    frame_rate(10)
     p = []
     background(0)
     load_np_pixels()
@@ -28,39 +33,29 @@ def evaluate_neighbors():
     for x in range(1, width - 1):
         for y in range(1, height - 1):
             count = 0
-            if np.array_equal(np_pixels[y - 1, x - 1,:], state[1]):
-                count+=1
-            if np.array_equal(np_pixels[y, x - 1,:], state[1]):
-                count+=1
-            if np.array_equal(np_pixels[y + 1, x - 1,:], state[1]):
-                count+=1
-            if np.array_equal(np_pixels[y + 1, x,:], state[1]):
-                count+=1
-            if np.array_equal(np_pixels[y - 1, x,:], state[1]):
-                count+=1
-            if np.array_equal(np_pixels[y - 1, x + 1,:], state[1]):
-                count+=1
-            if np.array_equal(np_pixels[y, x + 1,:], state[1]):
-                count+=1
-            if np.array_equal(np_pixels[y + 1, x + 1,:], state[1]):
-                count+=1
-            if count < 2:
-                p[y][x] = state[0]
-            elif count == 3:
+            if np_pixels[y - 1, x + 1, 2] == 255: count+=1
+            if np_pixels[y - 1, x, 2] == 255: count+=1
+            if np_pixels[y - 1, x - 1, 2] == 255: count+=1
+            if np_pixels[y, x + 1, 2] == 255: count+=1
+            if np_pixels[y, x - 1, 2] == 255: count+=1
+            if np_pixels[y + 1, x + 1, 2] == 255: count+=1
+            if np_pixels[y + 1, x, 2] == 255: count+=1
+            if np_pixels[y + 1, x - 1, 2] == 255: count+=1
+            if count == 3:
                 p[y][x] = state[1]
-            elif count == 2 and p[y][x] == state[1]:
-                p[y][x] = state[1]
-            else:
+            elif count < 2 or count > 3:
                 p[y][x] = state[0]
             
 
 def transition():
     global p
-    for y in range(height):
-        for x in range(width):
+    for x in range(width):
+        for y in range(height):
             np_pixels[y, x, :] = p[y][x] 
     update_np_pixels()
     
 def draw():
+    #if frame_count % 20 == 0:
+    #    draw_glider()
     evaluate_neighbors()
     transition()
